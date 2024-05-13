476363ce88c580a5d925d30dc0c2eca049e3867c
VasukPasuk <vasapaskivskij@gmail.com>
2024-05-13 23:25:08 +0300

    Adaptive design

--- src/Components/SumoIntroductionBlock/SumoIntroduction.block.jsx
+++ src/Components/SumoIntroductionBlock/SumoIntroduction.block.jsx
@@ -20,7 +20,7 @@ function SumoIntroductionBlock(props) {
       </div>
       <div className="sumo-introduction-block-img-wrapper">
         <div className={`sumo-introduction-block-img-box`}>
-          <img src="./robot-fighting-3.png" alt="robot fight image"/>
+          <img src="robot-fighting-3.png" alt="robot fight image"/>
           <div className={`sumo-introduction-img-shadow`}/>
         </div>
       </div>
