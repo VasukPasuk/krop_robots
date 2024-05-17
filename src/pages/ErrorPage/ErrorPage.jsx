e8639c2528f719f4d90de355d0889d9cd62be123
VasukPasuk <vasapaskivskij@gmail.com>
2024-05-17 14:27:14 +0300

    Error page changes

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './ErrorPage.style.scss';
+import {NavLink} from "react-router-dom";
+import {AiFillHome} from "react-icons/ai";
 
 function ErrorPage(props) {
   return (
-    <div>
-      Error Page
-    </div>
+    <section id="error-page">
+      <img src="not-found-image.png" alt="not found image"/>
+      <p className="error-text-block">
+        Сторінка не була знайдена
+      </p>
+      <NavLink id="error-link-block" to={'/home'}>
+        Повернутися на головну сторінку
+      </NavLink>
+    
+    
+    </section>
   );
 }
 
